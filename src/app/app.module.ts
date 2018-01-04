c1917d261aac902158a655d21c22eeff60d6debb
Aljon Rey Cadiz <aljonrey.cadiz@yahoo.com>
2018-01-04 15:48:15 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUwWn-lX9hARggO6M27ismgdB3PAAJsHE",
+  authDomain: "auth-poc-1835e.firebaseapp.com",
+  databaseURL: "https://auth-poc-1835e.firebaseio.com",
+  projectId: "auth-poc-1835e",
+  storageBucket: "auth-poc-1835e.appspot.com",
+  messagingSenderId: "737884417586"
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
